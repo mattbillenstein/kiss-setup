f2ac4b154121697a7aab0670fb70f2e0b1c26ee7
Matt Billenstein <matt@vazor.com>
2020-06-21 05:57:45 +0000

    Don't hardcode sums, clean and download every execution

--- stage1.sh
+++ stage1.sh
@@ -1,17 +1,27 @@
 #!/bin/bash -xv
 
+set -eo pipefail
+
 LOOP="/dev/loop7"
 ROOT="/dev/loop7p1"
 
-set -eo pipefail
+VERSION="1.11.0"
+if [ "$1" != "" ]; then
+    VERSION="$1"
+fi
 
 sudo umount root || true
 sudo losetup -d $LOOP || true
 
-sudo rm -fR root
+sudo rm -fR root kiss-chroot* kiss.img
 mkdir root
 
-rm -f kiss.img
+wget https://github.com/kisslinux/repo/releases/download/${VERSION}/kiss-chroot.tar.xz
+wget https://raw.githubusercontent.com/kisslinux/kiss/master/contrib/kiss-chroot
+chmod 755 kiss-chroot
+
+wget https://github.com/kisslinux/repo/releases/download/${VERSION}/kiss-chroot.tar.xz.sha256
+sha256sum -c < kiss-chroot.tar.xz.sha256
 
 dd if=/dev/zero of=kiss.img bs=1G count=8
 
@@ -29,22 +39,6 @@ EOF
 sudo losetup -v -P $LOOP kiss.img
 sudo mkfs.ext4 $ROOT
 sudo mount $ROOT root
-
-if [ ! -e kiss-chroot.tar.xz ]; then
-    wget https://github.com/kisslinux/repo/releases/download/1.10.0/kiss-chroot.tar.xz
-    wget https://raw.githubusercontent.com/kisslinux/kiss/master/contrib/kiss-chroot
-    chmod 755 kiss-chroot
-fi
-
-if [ "$(sha256sum kiss-chroot.tar.xz | awk '{print $1}')" != "daf6858be86a4df76214cbbb41c0d8cd0992799deff667e7fd0b937c6bacc933" ]; then
-    echo "Bad sum"
-    exit 1
-fi
-
-if [ "$(sha256sum kiss-chroot | awk '{print $1}')" != "774c6f31cc938acae1df87cc81221b14137828f0c9e24d7120fd44b961070c91" ]; then
-    echo "Bad sum"
-    exit 1
-fi
 
 sudo tar xf kiss-chroot.tar.xz -C root --strip-components 1
 
