f713c89dda11a53028d59aa6bdbcc015bb8af843
Matt Billenstein <matt@vazor.com>
2020-08-24 21:00:43 +0000

    Update to 2020.7

--- stage1.sh
+++ stage1.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 LOOP="/dev/loop7"
 ROOT="/dev/loop7p1"
 
-VERSION="1.11.0"
+VERSION="2020.7"
 if [ "$1" != "" ]; then
     VERSION="$1"
 fi
@@ -16,12 +16,10 @@ sudo losetup -d $LOOP || true
 sudo rm -fR root kiss-chroot* kiss.img
 mkdir root
 
-wget https://github.com/kisslinux/repo/releases/download/${VERSION}/kiss-chroot.tar.xz
-wget https://raw.githubusercontent.com/kisslinux/kiss/master/contrib/kiss-chroot
-chmod 755 kiss-chroot
+wget https://github.com/kisslinux/repo/releases/download/${VERSION}/kiss-chroot-${VERSION}.tar.xz
 
-wget https://github.com/kisslinux/repo/releases/download/${VERSION}/kiss-chroot.tar.xz.sha256
-sha256sum -c < kiss-chroot.tar.xz.sha256
+wget https://github.com/kisslinux/repo/releases/download/${VERSION}/kiss-chroot-${VERSION}.tar.xz.sha256
+sha256sum -c < kiss-chroot-${VERSION}.tar.xz.sha256
 
 dd if=/dev/zero of=kiss.img bs=1G count=8
 
@@ -40,13 +38,13 @@ sudo losetup -v -P $LOOP kiss.img
 sudo mkfs.ext4 $ROOT
 sudo mount $ROOT root
 
-sudo tar xf kiss-chroot.tar.xz -C root --strip-components 1
+sudo tar xf kiss-chroot-${VERSION}.tar.xz -C root --strip-components 1
 
 sudo cp stage2.sh root/
 
 echo "Run ./stage2.sh in the chroot..."
 
-sudo ./kiss-chroot ./root
+sudo root/bin/kiss-chroot ./root
 
 sudo umount root
 sudo losetup -d $LOOP
